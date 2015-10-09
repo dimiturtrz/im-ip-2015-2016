512a0427c8c186a0be0cc4df7c87325a41c15823
dimiturtrz <dimitar.trz@gmail.com>
2015-10-09 18:04:30 +0300

    Classes

--- 01_SampleJava/src/ClassesExample.java
+++ 01_SampleJava/src/ClassesExample.java
@@ -3,6 +3,14 @@ public class ClassesExample {
 	public static void main(String[] args) {
 		// CTRL+2 -> L = local variable
 		final Room room = new Room();
+
+		room.setHeight(10);
+		room.setWidth(12);
+		
+		room.getHeight();
+		room.getWidth();
+		
+		System.out.println(room.calculateArea());
 	}
 	
 }
--- 01_SampleJava/src/Room.java
+++ 01_SampleJava/src/Room.java
@@ -1,3 +1,30 @@
 public class Room {
+	// define fields
+	private int width;
+	private int height;
 	
+	// Source->Generate... || 
+	// SHIFT+ALT+S -> R = Getters and Setters
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		// check value of width
+		if(width>0)
+			this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		if(height>0)
+			this.height = height;
+	}
+	
+	// CTRL+SPACE  = method
+	public int calculateArea() {
+		// CTRL+1 = quickfix(return type)
+		return height*width;
+
+	}
 }
