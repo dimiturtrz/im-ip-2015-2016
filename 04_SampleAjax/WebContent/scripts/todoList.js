f79e26d2e9a1658324b19d52d1c25c5771db6da8
dimiturtrz <dimitar.trz@gmail.com>
2015-12-23 20:43:34 +0200

    adding project files with add, edit, delete working buttons

---
+++ 04_SampleAjax/WebContent/scripts/todoList.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+	"use strict";
+	var ENDPOINT = "http://localhost:3000/tasks";
+	function taskEndpoint(taskId) {
+		return ENDPOINT + "/" + taskId;
+	}
+
+	function showPanel(panelName) {
+		var ALL_PANELS = ["emptyPanel", "readPanel", "updatePanel", "createPanel"];
+		_.forEach(ALL_PANELS, function(nextValue) {
+			$("#"+nextValue).hide();
+		});
+		$("#"+panelName).show();
+	}
+
+	
+	// list tasks
+	function listTasks() {
+		return $.ajax(ENDPOINT, {
+			method: "GET",
+			dataType: "json"
+		});
+	}
+	
+	//create task
+	function addTask(task) {
+		return $.ajax(ENDPOINT, {
+			method: "POST",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify(task),
+			dataType: "json"
+		});
+	}
+
+	//read task
+	function readTask(taskId) {
+		return $.ajax(taskEndpoint(taskId), {
+			method: "GET",
+			dataType: "json"
+		});
+	}
+	
+	// delete task
+	function deleteTask(taskId){
+		$.ajax(taskEndpoint(taskId), {
+			method: "DELETE"
+		});
+	}
+	
+	// update task
+	function updateTask(task, taskId){
+		$.ajax(taskEndpoint(taskId), {
+			method: "PUT",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify(task),
+			dataType: "json"
+		})
+	}
+	
+	// read panel
+	function setupReadPanel(taskId){
+		readTask(taskId).then(showTaskView);
+		$("#readPanel .task-action-remove").click(function(){
+			deleteTask(taskId);
+		});
+		$("#readPanel .task-action-ok").click(function(){
+			setupEditPanel(taskId);
+		});
+	}
+	
+	// edit panel
+	function setupEditPanel(taskId){
+		showPanel("updatePanel");
+		$("#updatePanel input[name=title]").val($("#readPanel .panel-body .task-title").text());
+		$("#updatePanel textarea[name=description]").val($("#readPanel .panel-body .task-description").text());
+		$("#updatePanel .task-action-ok").click(function(){
+			var task = getTaskObject("update");
+			updateTask(task, taskId);
+			showPanel("emptyPanel");
+		});
+	}
+	
+	
+	// create panel
+	function setupCreatePanel(){
+		showPanel("createPanel");
+		$("#createPanel .task-action-ok").click(function() {
+			var task = getTaskObject("create");
+			addTask(task);
+		});
+	}
+	
+	function getTaskObject(inputPanel){
+		var panelSelector = "#" + inputPanel + "Panel ";
+		return {
+			title: $(panelSelector + "input[name=title]").val(),
+			description: $(panelSelector + "textarea[name=description]").val(),
+		}
+	}
+	
+	function showTaskView(task) {
+		$("#readPanel .task-title").text(task.title);
+		$("#readPanel .task-description").text(task.description);
+		showPanel("readPanel");
+	}
+	function reloadTasks() {
+		listTasks().then(function(response) {
+			function addTaskToList(task) {
+				var newItem = $("<li />");
+				newItem.text(task.title);
+				newItem.addClass("list-group-item");
+				newItem.attr("data-task-id", task.id);
+				$("#tasksList").append(newItem);
+			}
+			$("#tasksList").html("");
+			_.forEach(response, addTaskToList);
+		});
+	}
+	function attachHandlers() {
+		$(document).on("click", "#tasksList [data-task-id]", function() {
+			var taskId = $(this).attr("data-task-id");
+			setupReadPanel(taskId);
+		});
+		
+		$(document).on("click", "#addTaskButton", function() {
+			setupCreatePanel();
+		});
+		
+		$(".task-action-cancel").click(function() {
+			showPanel("emptyPanel");
+		});
+	}
+	attachHandlers();
+	reloadTasks();
+});
